a04ce98cde3ce744fc8a2d280c270c92bf1c6db6
Isa <theonewhohasthisemail@gmail.com>
2023-11-20 17:55:47 +0000

    Redo watchers to be threaded and loaded by run

---
+++ src/run.py
@@ -0,0 +1,30 @@
+import time, math, threading
+
+collectors = {}
+publishers = {}
+runningCollectors = {}
+runningPublishers = {}
+data = {"lastSync": 0, "test": {"offset": 1}, "test2": {"offset": 2}}
+
+def run(saveSync = 0):
+    """
+    Run the application.
+    saveSync: 0 = Save when all values are updated, 1 = save with cached data on updated data, 2 = save only updated values when a value is updated Only applies to collectors that support it.
+    """
+    data['lastSync'] = int(time.time())
+    for i in collectors:
+        runningCollectors[i] = threading.Thread(target=collectors[i]["function"], kwargs={"interval":collectors[i]["interval"], "repeat":collectors[i]["repeat"]}).start()
+        
+
+def updateTime():
+    """
+    Update the last sync time.
+    """
+    minOffset = math.inf
+    for item in data:
+        if item != 'lastSync' and data[item]["offset"] < minOffset:
+            minOffset = data[item]["offset"]
+    data['lastSync'] += minOffset
+    for item in data:
+        if item != 'lastSync':
+            data[item]["offset"] -= minOffset
--- src/watcher.py
+++ src/watcher.py
@@ -1,12 +1,15 @@
-import time, math
+import math, time, src.run as run
 
 def watcher(original_function, name, interval=1, repeat=math.inf):
     """Backend function for watcher decorator"""
-    count = 0
-    while count < repeat:
-        time.sleep(interval)
-        return_value = original_function()
-        count += 1
+    def collector(interval, repeat):
+        """Backend function for watcher decorator"""
+        count = 0
+        while count < repeat:
+            count += 1
+            original_function()
+            time.sleep(interval)
+    run.collectors[name] = {"function":collector, "interval": interval, "repeat": repeat}
 
 def newWatcher(interval=1, repeat=math.inf):
     """
